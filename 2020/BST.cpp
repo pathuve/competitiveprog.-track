de74f46c0ce08383b3b99f5947e3fad56e753945
Parth Banathia <61310729+Parth0105@users.noreply.github.com>
2021-10-01 10:12:26 +0530

    Update BST.cpp

--- 2020/BST.cpp
+++ 2020/BST.cpp
@@ -1,1 +1,68 @@
 
+int main()
+{
+   int c, n,item;
+   BST bst;
+   nod *t;
+   while (1)
+   {
+      cout<<"1.Insert Element "<<endl;
+      cout<<"2.Delete Element "<<endl;
+      cout<<"3.Search Element"<<endl;
+      cout<<"4.Inorder Traversal"<<endl;
+      cout<<"5.Preorder Traversal"<<endl;
+      cout<<"6.Postorder Traversal"<<endl;
+      cout<<"7.Display the tree"<<endl;
+      cout<<"8.Quit"<<endl;
+      cout<<"Enter your choice : ";
+      cin>>c;
+      switch(c)
+      {
+         case 1:
+            t = new nod;
+            cout<<"Enter the number to be inserted : ";
+            cin>>t→info;
+            bst.insert(r, t);
+            break;
+         case 2:
+            if (r == NULL)
+            {
+               cout<<"Tree is empty, nothing to delete"<<endl;
+               continue;
+            }
+            cout<<"Enter the number to be deleted : ";
+            cin>>n;
+            bst.del(n);
+            break;
+         case 3:
+            cout<<"Search:"<<endl;
+            cin>>item;
+            bst.search(r,item);
+            break;
+         case 4:
+            cout<<"Inorder Traversal of BST:"<<endl;
+            bst.inorder(r);
+            cout<<endl;
+            break;
+         case 5:
+            cout<<"Preorder Traversal of BST:"<<endl;
+            bst.preorder(r);
+            cout<<endl;
+            break;
+         case 6:
+            cout<<"Postorder Traversal of BST:"<<endl;
+            bst.postorder(r);
+            cout<<endl;
+            break;
+         case 7:
+            cout<<"Display BST:"<<endl;
+            bst.show(r,1);
+            cout<<endl;
+            break;
+         case 8:
+            exit(1);
+         default:
+            cout<<"Wrong choice"<<endl;
+      }
+   }
+}
