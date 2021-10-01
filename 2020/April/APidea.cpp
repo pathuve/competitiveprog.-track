e085d12016f8e153e91011abe8d1b6438913b40f
Parth Banathia <61310729+Parth0105@users.noreply.github.com>
2021-10-01 10:15:34 +0530

    Update APidea.cpp

--- 2020/April/APidea.cpp
+++ 2020/April/APidea.cpp
@@ -1,23 +1,36 @@
-#include<iostream>
+// C++ program to check if a given array
+// can form arithmetic progression
+#include<bits/stdc++.h>
 using namespace std;
+
+// Returns true if a permutation of arr[0..n-1]
+// can form arithmetic progression
+bool checkIsAP(int arr[], int n)
+{
+if (n == 1)
+	return true;
+
+// Sort array
+sort(arr, arr + n);
+
+// After sorting, difference between
+// consecutive elements must be same.
+int d = arr[1] - arr[0];
+for (int i=2; i<n; i++)
+	if (arr[i] - arr[i-1] != d)
+	return false;
+
+return true;
+}
+
+// Driven Program
 int main()
 {
-    long long int t;
-    cin>>t;
-    while(t--)
-    {
-        long long int  sum,n,a,cd;//cd-common difference
-        cin>>sum>>n>>a;
-        cd=(((2*sum)/n)-2*a)/(n-1);//calculating common difference by A.P sum upto n formulae
-        for(int i=0;i<n;i++)
-        {
-            cout<<a<<" ";
-            a=a+cd;
-        }
+int arr[] = { 20, 15, 5, 0, 10 };
+int n = sizeof(arr)/sizeof(arr[0]);
 
+(checkIsAP(arr, n))? (cout << "Yes" << endl) :
+					(cout << "No" << endl);
 
-
-    }
-
-
+return 0;
 }
